b4a041745ded2f65e980340d9fc40cb4a9b23770
seth <seth@eventespresso.com>
2012-12-06 02:21:50 -0700

    Adding a title to the event title and description fields. Adds pricing and attendee limits section. [touch:1066, 1752, 1756]

--- templates/fem_form_output.php
+++ templates/fem_form_output.php
@@ -15,16 +15,24 @@ function ee_fem_form_output() {
 				<?php _e('Event Information', 'event_espresso'); ?>
 			</h3>
 			<div class="event-data-display <?php echo $use_themeroller == TRUE ? 'ui-widget-content ui-corner-bottom':''; ?>">
-				<p class="event_form_field ee_fem_form_field">
-					<label class="screen-reader-text" for="event_title_field">
-						<?php _e('Event Title', 'event_espresso'); ?>
-						<em>*</em> </label>
-					<input class="required" name="event" size="30" tabindex="1" value="" id="event_title_field" type="text">
-				</p>
-				<p class="event_form_field ee_fem_form_field">
-					<label class="screen-reader-text" for="event_desc">
+				<div class="event_form_field ee_fem_form_field title">
+					<label class="" for="event_title_field">
+						<?php _e('Event Title', 'event_espresso'); ?><em>*</em></label>
+					<br />
+					<input class="required" name="event" tabindex="1" value="" id="event_title_field" type="text">
+									</div>
+				<div class="event_form_field ee_fem_form_field reg_limit">
+					<label class="ee_fem-reg-limit-label" for="reg_limit">
+						<?php _e('Registration Limit', 'event_espresso'); ?></label>
+					<br />
+					<input class="ee_fem-reg-limit-input" name="reg_limit" tabindex="1" value="" id="reg_limit" type="text">
+				</div>
+				<div class="clear"></div>
+				<p class="event_form_field ee_fem_form_field title">
+					<label class="" for="event_desc">
 						<?php _e('Description', 'event_espresso'); ?>
 					</label>
+					<br />
 					<?php $args = array("textarea_rows" => 5, "textarea_name" => "event_desc", "editor_class" => "my_editor_custom");
 				wp_editor("", "event_desc", $args);?>
 				</p>
@@ -37,19 +45,19 @@ function ee_fem_form_output() {
 			<div class="event-data-display <?php echo $use_themeroller == TRUE ? 'ui-widget-content ui-corner-bottom':''; ?>">
 				<p class="event_form_field ee_fem_form_field">
 					<label class="date_time" for="registration_start"><?php echo __('Start Date', 'event_espresso') ?><em>*</em> </label>
-					<input type="text" size="10" id="registration_start" class="datepicker required ee_fem_date_value" name="registration_start" value="<?php echo date('Y-m-d'); ?>" />
+					<input type="text" id="registration_start" class="datepicker required ee_fem_date_value" name="registration_start" value="<?php echo date('Y-m-d'); ?>" />
 					<label class="date_time time" for="add-reg-start">
 						<?php _e('Time', 'event_espresso'); ?>
 						<em>*</em> </label>
-					<input class="required ee_fem_time_value" size="10" type="text" id="add-reg-start" name="registration_startT" value="12:01 AM" />
+					<input class="required ee_fem_time_value" type="text" id="add-reg-start" name="registration_startT" value="12:01 AM" />
 				</p>
 				<p class="event_form_field ee_fem_form_field">
 					<label class="date_time" for="registration_end"> <?php echo __('End Date', 'event_espresso') ?><em>*</em></label>
-					<input type="text" size="10" id="registration_end" class="datepicker required ee_fem_date_value" name="registration_end" value="<?php echo  date('Y-m-d',time() + (60 * 60 * 24 * 29)); ?>" />
+					<input type="text"  id="registration_end" class="datepicker required ee_fem_date_value" name="registration_end" value="<?php echo  date('Y-m-d',time() + (60 * 60 * 24 * 29)); ?>" />
 					<label class="date_time time" for="registration_endT">
 						<?php _e('Time', 'event_espresso'); ?>
 						<em>*</em> </label>
-					<input class="required ee_fem_time_value" size="10" type="text" id="registration_endT" name="registration_endT" value="11:59 PM" />
+					<input class="required ee_fem_time_value" type="text" id="registration_endT" name="registration_endT" value="11:59 PM" />
 					<br />
 					<span class="description">
 					<?php _e('All events <strong>require</strong> registration start/end dates and start/end times in order to display properly on your pages.', 'event_espresso'); ?>
@@ -74,25 +82,56 @@ function ee_fem_form_output() {
 				<p class="event_form_field ee_fem_form_field">
 					<label class="date_time" for="start_date">
 						<?php  _e('Start Date', 'event_espresso') ?><em>*</em></label>
-					<input type="text" size="10" id="start_date" class="datepicker required ee_fem_date_value" name="start_date" value="<?php echo date('Y-m-d',time() + (60 * 60 * 24 * 30)); ?>" />
+					<input type="text"  id="start_date" class="datepicker required ee_fem_date_value" name="start_date" value="<?php echo date('Y-m-d',time() + (60 * 60 * 24 * 30)); ?>" />
 					<label class="date_time time" for="add-start-time">
-						<?php _e('Time', 'event_espresso'); ?><em>*</em></label>
-					<input class="required ee_fem_time_value" size="10" type="text" id="add-start-time" name="start_time[]" value="8:00 AM" />
+						<?php _e('Time', 'event_espresso'); ?>
+						<em>*</em></label>
+					<input class="required ee_fem_time_value"  type="text" id="add-start-time" name="start_time[]" value="8:00 AM" />
 				</p>
 				<p class="event_form_field ee_fem_form_field">
 					<label class="date_time" for="end_date">
-						<?php  _e('End Date', 'event_espresso') ?><em>*</em></label>
-					<input type="text" size="10" id="end_date" class="datepicker required ee_fem_date_value" name="end_date" value="<?php echo date('Y-m-d',time() + (60 * 60 * 24 * 30)); ?>" />
+						<?php  _e('End Date', 'event_espresso') ?>
+						<em>*</em></label>
+					<input type="text"  id="end_date" class="datepicker required ee_fem_date_value" name="end_date" value="<?php echo date('Y-m-d',time() + (60 * 60 * 24 * 30)); ?>" />
 					<label class="date_time time" for="add-end-time">
-						<?php _e('Time', 'event_espresso'); ?><em>*</em></label>
-					<input class="required ee_fem_time_value" size="10"  type="text" id="add-end-time" name="end_time[]" value="5:00 PM" />
+						<?php _e('Time', 'event_espresso'); ?>
+						<em>*</em></label>
+					<input class="required ee_fem_time_value"   type="text" id="add-end-time" name="end_time[]" value="5:00 PM" />
 					<br />
 					<span class="description">
 					<?php _e('All events <strong>require</strong> a start and end date in order to display properly on your pages.', 'event_espresso'); ?>
 					</span></p>
 			</div>
 		</div>
+		
+		<?php if (isset($org_options['fem_settings']['enable_fem_pricing_section']) && $org_options['fem_settings']['enable_fem_pricing_section'] == 'Y'): ?>
+		<!-- Pricing -->
+		<div id="add-pricing" class="event_data event-data-display event-display-boxes <?php echo $use_themeroller == TRUE ? 'ui-widget':''; ?>">
+			<?php (defined('EVENTS_MEMBER_REL_TABLE'))? $members_active = 'class="members-active"' : $members_active = ''; ?>
+			<h3 class="event_title <?php echo $use_themeroller == TRUE ? 'ui-widget-header ui-corner-top':''; ?>">
+				<?php _e('Event Pricing', 'event_espresso'); ?>
+			</h3>
+			<div class="event-data-display <?php echo $use_themeroller == TRUE ? 'ui-widget-content ui-corner-bottom':''; ?>">
+				<table class="fem-pricing-table" <?php echo $members_active ?>width="100%" border="0" cellpadding="5">
+					<tr valign="top">
+						<td id="standard-pricing" class="fem-pricing-td"><?php event_espresso_multi_price_update($event_id); //Standard pricing ?></td>
+						<?php
+							//If the members addon is installed, define member only event settings
+							if  (defined('EVENTS_MEMBER_REL_TABLE') && $espresso_premium == true) { ?>
+								<td id="member-pricing" class="b"><?php echo event_espresso_member_only_pricing(); //Show the the member only pricing options. ?></td>
+						<?php
+							}
+						?>
+					</tr>
+				</table>
+			</div>
+		</div>
+		
+		
+		<?php endif; ?>
+		
 		<?php if (isset($org_options['fem_settings']['enable_fem_category_select']) && $org_options['fem_settings']['enable_fem_category_select'] == 'Y'): ?>
+		<!-- Categories -->
 		<div id="add-categories" class="event_data event-data-display event-display-boxes <?php echo $use_themeroller == TRUE ? 'ui-widget':''; ?>">
 			<h3 class="event_title <?php echo $use_themeroller == TRUE ? 'ui-widget-header ui-corner-top':''; ?>">
 				<?php _e('Event Category', 'event_espresso'); ?>
@@ -262,4 +301,5 @@ function ee_fem_form_output() {
 	ob_end_clean();
 	return $buffer;
 }
+
 
