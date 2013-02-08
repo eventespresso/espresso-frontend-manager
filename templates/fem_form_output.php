aa98aee3018a735812eed6ddcf5fe1c9cd1ff5ed
Seth <seth@eventespresso.com>
2013-02-07 20:30:55 -0700

    Fixes Undefined index notice.
    [touch:2014]

--- templates/fem_form_output.php
+++ templates/fem_form_output.php
@@ -142,7 +142,7 @@ function ee_fem_form_output() {
 			</div>
 		</div>
 		<?php endif; ?>
-		<?php if ($use_venues == TRUE && $org_options['fem_settings']['enable_fem_venue_section'] == 'Y'):?>
+		<?php if ($use_venues == TRUE && isset($org_options['fem_settings']['enable_fem_venue_section']) && $org_options['fem_settings']['enable_fem_venue_section'] == 'Y'):?>
 		<div id="add-venue" class="event_data event-data-display event-display-boxes <?php echo $use_themeroller == TRUE ? 'ui-widget':''; ?>">
 			<h3 class="event_title <?php echo $use_themeroller == TRUE ? 'ui-widget-header ui-corner-top':''; ?>">
 				<?php _e('Event Venue', 'event_espresso'); ?>
